e10ac203962e0b22cab93d7cb1d53f817579b4e9
Jacob Anderson <jacubcub@gmail.com>
2022-07-15 14:42:23 -0600

    some display cleanup

--- wallet_search.py
+++ wallet_search.py
@@ -28,11 +28,20 @@ def get_initial_data():
 open_positions_df = get_initial_data()
 
 def submit_callback():
-    position_side = "LENDER" if st.session_state.user_action_select_input == "Deposited" else "BORROWER"
+    if st.session_state.user_action_select_input == "Deposited":
+        position_side = "LENDER"
+        position_side_column_label = "CURRENT DEPOSITED"
+    else:
+        position_side = "BORROWER"
+        position_side_column_label = "CURRENT BORROWED"
+
     filtered_df = open_positions_df[(open_positions_df["balance_adj"] > st.session_state.asset_quantity_input) 
                                     & (open_positions_df["market.inputToken.symbol"] == st.session_state.asset_select_input)
                                     & (open_positions_df["side"] == position_side)]    
     display_results = filtered_df[["account_id", "balance_adj", "market.inputToken.symbol", "balance_usd"]].copy()
+    display_results.sort_values("balance_adj", ascending=False, inplace=True)
+    display_results["balance_usd"] = display_results["balance_usd"].apply(lambda x: "${:,.2f}".format(x))
+    display_results.rename(columns={"account_id": "ADDRESS", "balance_adj": position_side_column_label, "market.inputToken.symbol": "ASSET", "balance_usd": "VALUE"}, inplace=True)
     st.session_state["results_df"] = display_results
 
 
@@ -48,7 +57,7 @@ with st.form("search_filter_form"):
     
 
 if submitted:
-    st.write("Showing wallets with more than", asset_quanity, asset_select)
+    st.write("Showing users that have", user_action_select.lower(), "more than", asset_quanity, asset_select)
 
 if not st.session_state["results_df"].empty:
-    st.write(st.session_state["results_df"])
+    st.dataframe(st.session_state["results_df"])
